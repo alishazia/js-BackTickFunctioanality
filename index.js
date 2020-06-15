5a71d9ffeb26f0ab243c3cf90f0242fe22446292
alishazia <shaziaali812@gmail.com>
2020-06-15 21:52:24 +0530

    1 file modified

--- index.js
+++ index.js
@@ -12,35 +12,47 @@ function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
-
 let currentResult = 0;
-
 currentResult = currentResult + (10 * 2) + 12 / 2;
-
-// passing a string here in the function
-// let currentDescription = '(10 * 2) + 12/2';
-
-// here we are using bactick and display a value as a string 
-let calculateValue = `(10 * 2) + 12/2`;
-
-// here we using backtick and rendering the dynamic value there using $ sign
-let currentDescription = `(${currentResult} * 2) + 12/2`;
-
-// implemeting line break using back tick along with CSS
-let currentDescription = `(${currentResult} * 2)
-
- + 12/2`;
-
-// here using normal qoutes for the string to use line break gives error
-//  let currentDescription = 'currentResult 
-
-//  + (10 * 2) + 12/2'
-
-// here using '+' operator to concatinate the strings we can use line break but will not display line break , only works with backtick
-let currentDescription = 'currentResult '
-  + '(10 * 2) + 12/2';
-
-// using escape character and css to render using '' there
-  let currentDescription = 'currentResult \n'
-  + '(10 * 2) + 12/2';
+let currentDescription = '(10 * 2) + 12/2';
 outputResult(currentResult, currentDescription)
+
+
+// about functions
+function displayMessage(a, b) {
+  return a + b;
+}
+displayMessage(10, 20);
+
+// function defined as expression and stored in a variable
+var x = function displayMessage(a, b) { return a + b };
+// variable used as function
+var y = x(4, 3);
+
+//  anonymus functions
+var x = function (a, b) { return a * b };
+var z = x(2, 3);   //invoking the anonymous function using variabble name
+
+
+//  self invoking anonymous functions
+(function () {
+  let a = 10, b = 10;
+  return a + b;
+})();
+
+
+//  self invoking anonymous functions
+(function myFunction() {
+  let a = 10, b = 10;
+  return a + b;
+})();
+
+// arrow functions
+// ES5 syntax
+var x = function (a, b) {
+  return a * b;
+}
+
+// ES6  syntax
+// using const here because function expression is always const,also omit function, return keyword, and curly braces there
+const x = (a, b) => a * b;
