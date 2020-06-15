9ffc502dea62185ab173ed1d19f83bd76edaa8e7
alishazia <shaziaali812@gmail.com>
2020-06-15 12:11:03 +0530

    2 files modified

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const divideBtn = document.getElementById('btn-divide');
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById
-('current-calculation');
+  ('current-calculation');
 
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
@@ -15,7 +15,7 @@ function outputResult(result, text) {
 
 let currentResult = 0;
 
-currentResult = currentResult + (10 * 2) + 12/2;
+currentResult = currentResult + (10 * 2) + 12 / 2;
 
 // passing a string here in the function
 // let currentDescription = '(10 * 2) + 12/2';
@@ -26,4 +26,21 @@ let calculateValue = `(10 * 2) + 12/2`;
 // here we using backtick and rendering the dynamic value there using $ sign
 let currentDescription = `(${currentResult} * 2) + 12/2`;
 
-outputResult(currentResult , currentDescription)
+// implemeting line break using back tick along with CSS
+let currentDescription = `(${currentResult} * 2)
+
+ + 12/2`;
+
+// here using normal qoutes for the string to use line break gives error
+//  let currentDescription = 'currentResult 
+
+//  + (10 * 2) + 12/2'
+
+// here using '+' operator to concatinate the strings we can use line break but will not display line break , only works with backtick
+let currentDescription = 'currentResult '
+  + '(10 * 2) + 12/2';
+
+// using escape character and css to render using '' there
+  let currentDescription = 'currentResult \n'
+  + '(10 * 2) + 12/2';
+outputResult(currentResult, currentDescription)
